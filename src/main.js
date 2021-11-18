9c6acb014456b9985d02d0837b60c186c8684560
marlenslmb <marlenslmb@gmail.com>
2021-11-18 12:43:37 +0600

    firebase full

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwRfMqCZfpfZFUzvox9VEKJWE-pNhTkrI",
+    authDomain: "authtest-98ccd.firebaseapp.com",
+    projectId: "authtest-98ccd",
+    storageBucket: "authtest-98ccd.appspot.com",
+    messagingSenderId: "426543320125",
+    appId: "1:426543320125:web:bbc8f2158dd1a7d18bee54"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
